599aaab32a950a6a9a8197817e2e25c37407c362
Abhinav <abhin.srh@gmail.com>
2025-07-19 19:22:00 +0530

    userSchema

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          v
+        );
+      },
+      message:
+        "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.",
+    },
+  },
+});
+
+module.exports = mongoose.model('Userschema', userSchema);
